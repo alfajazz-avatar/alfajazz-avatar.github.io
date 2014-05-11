e824fe4e78968635a8c66082812231c6160cef2e
Vladimir Zaslavskiy <bobah22@gmail.com>
2014-05-11 14:37:09 +0300

    fix

--- php/resizer.php
+++ php/resizer.php
@@ -6,7 +6,7 @@ $coords = json_decode($_POST['coords']);
 
 $photo = new abeautifulsite\SimpleImage();
 $photo->load($_FILES['file']['tmp_name']);
-$photo->crop($coords->x, $coords->y, $coords->x2, $coords->x2);
+$photo->crop($coords->x, $coords->y, $coords->x2, $coords->y2);
 $photo->resize(400, 400);
 
 echo $photo->output_base64();
